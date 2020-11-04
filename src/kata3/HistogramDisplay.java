a46d76704e2c1ee4574008e6c8659575689c52a6
Joaquin <Joaquin@localhost>
2020-11-04 09:43:37 +0000

    Kata 3 Versión 2

--- src/kata3/HistogramDisplay.java
+++ src/kata3/HistogramDisplay.java
@@ -9,9 +9,11 @@ import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.ui.ApplicationFrame;
 
 public class HistogramDisplay extends ApplicationFrame{
+    private final Histogram<String> histogram;
     
-    public HistogramDisplay(String title) {
+    public HistogramDisplay(String title,Histogram<String> histogram) {
         super(title);
+        this.histogram = histogram;
         this.setContentPane(createPanel());
         this.pack();
         
@@ -32,11 +34,9 @@ public class HistogramDisplay extends ApplicationFrame{
     private DefaultCategoryDataset createDataset(){
         DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
         
-        dataSet.addValue(23, "", "ulpgc.es");
-        dataSet.addValue(31, "", "dis.ulpgc.es");
-        dataSet.addValue(16, "", "hotmail.com");
-        dataSet.addValue(7, "", "ull.es");
-        dataSet.addValue(39, "", "gmail..com");
+        for (String key : histogram.keySet()){
+            dataSet.addValue(histogram.get(key),"",key);
+        }
         
         return dataSet;
     }
--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -3,7 +3,18 @@ package kata3;
 public class Kata3 {
 
     public static void main(String[] args) {
-        HistogramDisplay histo = new HistogramDisplay("Histogram");
+        Histogram<String> histogram = new Histogram();
+        
+        histogram.increment("ulpgc.es");
+        histogram.increment("dis.ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ull.es");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("hotmai.com");
+                
+        HistogramDisplay histo = new HistogramDisplay("Histogram", histogram);
+        
         histo.execute();
     }
     
